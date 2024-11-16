032e9e90dc88fe63973b6a6e8d1d7769a571ca02
Abdok <azkotb@gmail.com>
2024-11-16 15:59:26 +0200

    corrected the name of the static folder

--- mobikb_v1/settings.py
+++ mobikb_v1/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Directory where collectstatic gathers files
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # Directory where collectstatic gathers files
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # Additional static file directories
 
 # Default primary key field type
