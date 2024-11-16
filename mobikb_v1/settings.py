df557b1d6fa6f06c5442a5b08c95d7b441875895
Abdok <azkotb@gmail.com>
2024-11-16 16:06:24 +0200

    trying something

--- mobikb_v1/settings.py
+++ mobikb_v1/settings.py
@@ -132,8 +132,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # Directory where collectstatic gathers files
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Directory where collectstatic gathers files
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # Additional static file directories
 
 # Default primary key field type
